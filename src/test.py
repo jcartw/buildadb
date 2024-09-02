f7dda12ff2f54db380d912cd5f76d0e6a3f574de
Justin Cartwright <cartwright.76@gmail.com>
2024-09-02 07:01:22 -0500

    add test case for data persistence

--- src/test.py
+++ src/test.py
@@ -1,12 +1,22 @@
 from subprocess import PIPE, Popen
 import json
+import os
 
 # references:
 # - https://stackoverflow.com/questions/77802033/c-program-and-subprocess
 
+# clear test.db before each run of script
+DB_FILENAME = "test.db"
+def clear_db():
+    if os.path.isfile(DB_FILENAME):
+        os.remove(DB_FILENAME)
+
+clear_db()
+
 def run_script(commands):
+
     out = ""
-    p = Popen(["./db"], stdin=PIPE, stdout=PIPE, stderr=PIPE, text=True)
+    p = Popen([f"./db", DB_FILENAME], stdin=PIPE, stdout=PIPE, stderr=PIPE, text=True)
     try:
         for command in commands:
             p.stdin.write(command + "\n")
@@ -24,6 +34,7 @@ def equal_results(a, b):
 
 it = "inserts and retrieves a row"
 status = "FAILED"
+clear_db()
 result = run_script([
     'insert 1 user1 person1@example.com',
     'select',
@@ -43,6 +54,7 @@ print(f"{it}: {status}")
 
 it = "prints error message when table is full"
 status = "FAILED"
+clear_db()
 statements = []
 for i in range(1401):
     num = str(i + 1)
@@ -58,6 +70,7 @@ print(f"{it}: {status}")
 
 it = "allows inserting strings that are the maximum length"
 status = "FAILED"
+clear_db()
 long_username = "a" * 32
 long_email = "a" * 255
 
@@ -79,6 +92,7 @@ print(f"{it}: {status}")
 
 it = "prints error message if strings are too long"
 status = "FAILED"
+clear_db()
 long_username = "a" * 33
 long_email = "a" * 256
 
@@ -101,6 +115,7 @@ print(f"{it}: {status}")
 
 it = "prints an error message if id is negative"
 status = "FAILED"
+clear_db()
 
 result = run_script([
     f"insert -1 cstack foo@bar.com",
@@ -114,5 +129,36 @@ expectation = [
 ]
 if equal_results(result, expectation):
     status = "PASSED"
+print(f"{it}: {status}")
+
+
+# ----------------------------------------------------- #
+
+it = "keeps data after closing connection"
+status = "PASSED"
+clear_db()
+
+result = run_script([
+    f"insert 1 user1 person1@example.com",
+    '.exit'
+])
+expectation = [
+    "db > Executed.",
+    "db > "
+]
+if not equal_results(result, expectation):
+    status = "FAILED"
+
+result = run_script([
+    f"select",
+    '.exit'
+])
+expectation = [
+    "db > (1, user1, person1@example.com)",
+    "Executed."
+]
+if not equal_results(result, expectation):
+    status = "FAILED"
+
 print(f"{it}: {status}")
 
